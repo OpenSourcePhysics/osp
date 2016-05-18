314b263cf6d0592a816a58951ed3ff4e2aa4c559
Doug Brown <dobrown@cabrillo.edu>
2016-05-18 11:53:03 -0700

    Improve data parsing from text with extra delimiters (ie empty of data)

--- src/org/opensourcephysics/tools/DataTool.java
+++ src/org/opensourcephysics/tools/DataTool.java
@@ -950,16 +950,22 @@ public class DataTool extends OSPFrame implements Tool, PropertyChangeListener {
             }
           }
           // set column names if not yet set (null), String[] length > 0,
-          // all entries are NaN and none is "", and no data yet loaded
+          // all entries are NaN, and no data yet loaded
           if(rows.isEmpty()&&(strings.length>0)&&(columnNames==null)) {
             boolean valid = true;
             for(int k = 0; k<strings.length; k++) {
-              if(!Double.isNaN(rowData[k])||strings[k].equals("")) {                                                                   //$NON-NLS-1$
+              if(!Double.isNaN(rowData[k])) {
                 valid = false;
                 break;
               }
             }
             if(valid) {
+            	// replace "" with "?"
+            	for (int k=0; k<strings.length; k++) {
+            		if ("".equals(strings[k])) { //$NON-NLS-1$
+            			strings[k] = "?"; //$NON-NLS-1$
+            		}
+            	}
               columnNames = strings;
               columns = strings.length;
               textLine = input.readLine();
