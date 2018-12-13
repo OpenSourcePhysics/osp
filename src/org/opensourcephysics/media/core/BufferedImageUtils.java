8c1e8b1b2e7347b0a72dab0c9743d88e2795add5
NickKolok <nickkolok@mail.ru>
2018-12-13 19:35:53 +0300

    Just an annotation

--- src/org/opensourcephysics/media/core/BufferedImageUtils.java
+++ src/org/opensourcephysics/media/core/BufferedImageUtils.java
@@ -3,6 +3,13 @@ package org.opensourcephysics.media.core;
 import java.awt.image.BufferedImage;
 
 public class BufferedImageUtils {
+	/**
+	 * If source image has the desired type, does nothing and returns source.
+	 * Othrwise, creates a copy of source imagewith desired type and returns the copy.
+	 * @param source Image to be converted
+	 * @param type Type to convert to
+	 * @return Original or converted image
+	 */
 	public static BufferedImage convertIfNeeded(BufferedImage source, int type){
 		if (source.getType() != type) {
 			BufferedImage bi = new BufferedImage(source.getWidth(), source.getHeight(), type);
