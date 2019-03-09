6c30658c48e59ea7eaf9b7a5acd45a565cdc04ff
Doug Brown <dobrown@cabrillo.edu>
2019-03-09 10:12:38 -0800

    Update version to 5.0.7

--- src/org/opensourcephysics/display/OSPRuntime.java
+++ src/org/opensourcephysics/display/OSPRuntime.java
@@ -50,7 +50,7 @@ import org.opensourcephysics.tools.Translator;
  * @version 1.0
  */
 public class OSPRuntime {
-  public static final String VERSION = "5.0.2";                                                                            //$NON-NLS-1$
+  public static final String VERSION = "5.0.7";                                                                            //$NON-NLS-1$
   public static final String COMMA_DECIMAL_SEPARATOR = ",";                                                                            //$NON-NLS-1$
   public static final String PERIOD_DECIMAL_SEPARATOR = ".";                                                                            //$NON-NLS-1$
 
