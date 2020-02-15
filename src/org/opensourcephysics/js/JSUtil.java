64de0d49a20a7c6e9758ad2fa36ed5ca8cc7b77f
BobHanson <hansonr@stolaf.edu>
2020-02-15 11:13:09 -0500

    adds additional "known CORS-accessible" sites

--- src/org/opensourcephysics/js/JSUtil.java
+++ src/org/opensourcephysics/js/JSUtil.java
@@ -1,6 +1,20 @@
 package org.opensourcephysics.js;
 
 public class JSUtil {
+	static {
+		String[] corsEnabled = new String[] {
+				 "www.physlets.org", 
+// TODO				 "www.opensourcephysics.org",
+// TODO				 "www.compadre.org" 
+				 };
+				
+		for (int i = corsEnabled.length; --i >= 0;) {
+			String uri = corsEnabled[i];
+		/**
+		 * @j2sNative J2S.addDirectDatabaseCall(uri);
+		 */
+		}
+	}
 	
 	static public boolean isJS = /** @j2sNative true || */ false;
 
