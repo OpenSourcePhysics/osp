8246184f3fa8b6fdf1d90bbb4328f54939df04d6
BobHanson <hansonr@stolaf.edu>
2020-02-15 11:23:32 -0500

    fixed asynchronous file chooser

--- src/debugging/GetFileWithChooser.java
+++ src/debugging/GetFileWithChooser.java
@@ -4,19 +4,24 @@ import javax.swing.JFileChooser;
 
 import org.opensourcephysics.display.OSPRuntime;
 
+import javajs.async.AsyncFileChooser;
+
 public class GetFileWithChooser {
 	
 	
-	GetFileWithChooser(){
-    JFileChooser chooser = OSPRuntime.getChooser();
-    //JFileChooser chooser = new JFileChooser();
-    int result = chooser.showOpenDialog(null);
-    if(result==JFileChooser.APPROVE_OPTION) {
-      String fileName = chooser.getSelectedFile().getAbsolutePath();
-      System.out.println("File name="+fileName);
-    }
-	}
+	GetFileWithChooser() {
+		AsyncFileChooser chooser = OSPRuntime.getChooser();
+		chooser.showOpenDialog(null, new Runnable() {
 
+			@Override
+			public void run() {
+				String fileName = chooser.getSelectedFile().getAbsolutePath();
+				System.out.println("File name=" + fileName);
+			}
+
+		}, null);
+	}
+ 
   public static void main(String[] args) {
   	new GetFileWithChooser();
   }
