3f7fb483b6ba2b2d996c1edd785909c0f0d17c21
BobHanson <hansonr@stolaf.edu>
2020-03-25 10:10:37 -0500

    adds build_osp_zip; minor optimization of ResizableIcon

--- src/org/opensourcephysics/display/ResizableIcon.java
+++ src/org/opensourcephysics/display/ResizableIcon.java
@@ -7,6 +7,7 @@
 package org.opensourcephysics.display;
 
 import java.awt.AlphaComposite;
+import java.awt.Color;
 import java.awt.Component;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
@@ -23,9 +24,15 @@ import javax.swing.ImageIcon;
  */
 public class ResizableIcon implements Icon {
 
-  protected int baseWidth, baseHeight, w, h;
-  protected BufferedImage baseImage;
-  protected Icon icon;
+  // BH 2020.03.25 - made base fields final and all private for sanity.
+  // If necessary, could add getters and setters for those.
+  // But SwingJS may be problematic with that. Untested.
+	
+  private final int baseWidth, baseHeight;
+  private BufferedImage baseImage;
+  private final Icon icon;
+
+  private int w, h;
 
   /**
    * Creates a <CODE>ResizableIcon</CODE> from the specified URL.
@@ -36,42 +43,48 @@ public class ResizableIcon implements Icon {
     this(new ImageIcon(location));
   }
 
-  /**
-   * Creates a <CODE>ResizableIcon</CODE> from the specified Icon.
-   * 
-   * @param the icon to resize
-   */
-  public ResizableIcon(Icon icon) {
-  	// prevent nesting resizable icons
-  	while (icon instanceof ResizableIcon) {
-  		icon = ((ResizableIcon)icon).icon;
-  	}
-  	if(icon==null) {
-  		baseWidth = w=0;
-  		baseHeight = h=0;
-  	}else {
-	    this.icon = icon;
-	    baseWidth = w = icon.getIconWidth();
-	    baseHeight = h = icon.getIconHeight();
-  	}
-  }
+	/**
+	 * Creates a <CODE>ResizableIcon</CODE> from the specified Icon.
+	 * 
+	 * @param the icon to resize
+	 */
+	public ResizableIcon(Icon icon) {
+		// prevent nesting resizable icons
+		while (icon instanceof ResizableIcon) {
+			icon = ((ResizableIcon) icon).icon;
+		}
+		this.icon = icon;
+		if (icon == null) {
+			baseWidth = w = 0;
+			baseHeight = h = 0;
+		} else {
+			baseWidth = w = icon.getIconWidth();
+			baseHeight = h = icon.getIconHeight();
+		}
+	}
 
-  @Override
-  public synchronized void paintIcon(Component c, Graphics g, int x, int y) {
-    if (icon == null) {
-      return;
-    }
-    if (baseImage==null || baseImage.getWidth()!=baseWidth || baseImage.getHeight()!=baseHeight) {
-    	baseImage = new BufferedImage(baseWidth, baseHeight, BufferedImage.TYPE_INT_ARGB);    	
-    }
-    Graphics2D g2 = baseImage.createGraphics();
-    g2.setComposite(AlphaComposite.Clear); 
-    g2.fillRect(0, 0, baseWidth, baseHeight); 
-    g2.setComposite(AlphaComposite.SrcOver);
-    icon.paintIcon(c, g2, 0, 0);
-    g2.dispose();
-    g.drawImage(baseImage, x, y, w, h, c);
-  }
+	@Override
+	public synchronized void paintIcon(Component c, Graphics g, int x, int y) {
+		if (icon == null) {
+			return;
+		}
+		if (baseImage == null 
+// BH baseImage is final -- or should be??
+//				|| baseImage.getWidth() != baseWidth 
+//				|| baseImage.getHeight() != baseHeight
+				) {
+			baseImage = new BufferedImage(baseWidth, baseHeight, BufferedImage.TYPE_INT_ARGB);
+		}
+		Graphics2D g2 = baseImage.createGraphics();
+		// BH 2020.03.25 It is not necessary to clear the image first -- image will be
+		// AlphaComposit.SrcOver by default
+		// g2.setComposite(AlphaComposite.Clear);
+		// g2.fillRect(0, 0, baseWidth, baseHeight);
+		// g2.setComposite(AlphaComposite.SrcOver);
+		icon.paintIcon(c, g2, 0, 0);
+		g2.dispose();
+		g.drawImage(baseImage, x, y, w, h, c);
+	}
 
   @Override
   public int getIconWidth() {
@@ -92,15 +105,15 @@ public class ResizableIcon implements Icon {
   	return icon;
   }
   
-  /**
-   * Magnifies the icon by a specified integer factor.
-   * 
-   * @param factor the factor
-   */
-  public void resize(int factor) {
-  	int n = Math.max(factor, 1);
-  	w = n*baseWidth;
-  	h = n*baseHeight;
-  }
+	/**
+	 * Magnifies the icon by a specified integer factor.
+	 * 
+	 * @param factor the factor
+	 */
+	public void resize(int factor) {
+		int n = Math.max(factor, 1);
+		w = n * baseWidth;
+		h = n * baseHeight;
+	}
   
 }
