ee211c80e5e444b4c7a04709e21aee3eedef31d1
BobHanson <hansonr@stolaf.edu>
2020-04-26 14:31:55 -0500

    fixed JSUtil for saving opened files in /TEMP/OPEN/; adds video registration in PlayVideoTest

--- src/test/PlayVideoTest.java
+++ src/test/PlayVideoTest.java
@@ -1,6 +1,7 @@
 package test;
 
 import org.opensourcephysics.media.core.*;
+import org.opensourcephysics.media.mov.MovieFactory;
 
 
 public class PlayVideoTest {
@@ -8,6 +9,7 @@ public class PlayVideoTest {
 	
 	public static void main(String[] args) {
 		// register JSVideoTypes with VideoIO here
+		MovieFactory.registerWithViewoIO();
 		VideoPanel panel = new VideoPanel();
 		VideoFrame frame = new VideoFrame(panel);
 		frame.setVisible(true);
