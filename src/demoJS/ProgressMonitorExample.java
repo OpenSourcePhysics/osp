3fc7c3ab04a9617396af2ff49b5367c306f58c01
BobHanson <hansonr@stolaf.edu>
2020-05-01 18:31:40 -0500

    transpiler fix for nested Lamba methods; not actually used, I think.

--- src/demoJS/ProgressMonitorExample.java
+++ src/demoJS/ProgressMonitorExample.java
@@ -14,45 +14,62 @@ import javajs.async.AsyncSwingWorker;
 
 public class ProgressMonitorExample {
 
-	  private static ActionListener createStartTaskActionListener(Component parent) {
-	      UIManager.put("ProgressMonitor.progressText", "Test Progress");
-	      return (actionEvent) -> {
-	    	  
-        	  new AsyncSwingWorker(parent, "Test Task", 200, 1, 100) {
+	/**
+	 * SwingJS variant is just an anonymous subclass of AsyncSwingWorker.
+	 * 
+	 * The for-loop range [1,100] is set in the constructor. 
+	 * 
+	 * the body of the for loop is in doInBackground(i), but actually
+	 * nothing is done here; index i (the progress field) is just incremented. 
+	 * 
+	 * The iteration delay is set to 200 ms, same as Java.
+	 * 
+	 * The label is set using getNote().
+	 * 
+	 * In Java there is a final setting of the ProgressMonitor note, but that is meaningless, as its
+	 * dialog has been closed already.
+	 * 
+	 * @param parent
+	 * @return
+	 */
+	private static ActionListener createStartTaskActionListener(Component parent) {
+		UIManager.put("ProgressMonitor.progressText", "Test Progress");
+		return (actionEvent) -> {
+			new AsyncSwingWorker(parent, "Test Task", 200, 1, 100) {
 
-        			@Override
-        			public void initAsync() {
-        			    progressMonitor.setMillisToDecideToPopup(100);
-        			    progressMonitor.setMillisToPopup(100);
-        			}
+				@Override
+				public void initAsync() {
+					progressMonitor.setMillisToDecideToPopup(100);
+					progressMonitor.setMillisToPopup(100);
+				}
 
-        			@Override
-        			public int doInBackgroundAsync(int i) {
-        				// nothing to do in this demo except increment i
-        				return ++i;
-        			}
+				@Override
+				public int doInBackgroundAsync(int i) {
+					// nothing to do in this demo except increment i
+					return ++i;
+				}
 
-        			@Override
-        			public void doneAsync() {
-        				// nothing to do when done for this demo (the ProgressManager is closed already)
-        			}
-        			
-        			@Override
-        			public String getNote(int progress) {
-        				return "Task step: " + progress;
-        			}
-        		}.execute();
-	      };
-	  }
+				@Override
+				public void doneAsync() {
+					// nothing to do when done for this demo (the ProgressManager is closed already)
+				}
 
+				@Override
+				public String getNote(int progress) {
+					return "Task step: " + progress;
+				}
+			}.execute();
+		};
+	}
 
-	  
-	  
+//
+//	  
+//	  
 //	  private static ActionListener createStartTaskActionListenerOrig(Component parent) {
 //		      //for progress monitor dialog title
 //		      UIManager.put("ProgressMonitor.progressText", "Test Progress");
 //		      return (ae) -> {
-//		          new Thread(() -> {
+//		          new Thread(() -> { 
 //		              //creating ProgressMonitor instance
 //		              ProgressMonitor pm = new ProgressMonitor(parent, "Test Task",
 //		                      "Task starting", 0, 100);
@@ -77,7 +94,7 @@ public class ProgressMonitorExample {
 //		          }).start();
 //		      };
 //		  }
-//
+
 	
   public static void main(String[] args) {
       JFrame frame = createFrame("ProgressMonitor Example");
