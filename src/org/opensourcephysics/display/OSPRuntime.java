d118afe8d7b8f1e1753f1bf5eadbb71161ea36da
BobHanson <hansonr@stolaf.edu>
2020-05-03 09:09:57 -0500

    adds OSPRuntime.isBHTest

--- src/org/opensourcephysics/display/OSPRuntime.java
+++ src/org/opensourcephysics/display/OSPRuntime.java
@@ -102,10 +102,9 @@ public class OSPRuntime {
 		}
 	}
 	
-	public static boolean bhTest = false;
+	public static boolean isBHTest = false;
 	
-	
-	public static boolean allowSetFonts = bhTest || !isJS; // for testing
+	public static boolean allowSetFonts = !isBHTest || !isJS; // for testing
 
 	public static boolean allowAsyncURL = isJS; // for OSPRuntime and Library SwingWorkers
 	
