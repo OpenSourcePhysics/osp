abb7f7dffe02be8288f8ca8c0065f4c8563edf69
Douglas Brown <dobrown@cabrillo.edu>
2020-05-03 09:00:12 -0700

    change PluginVideoI to SmoothPlayable, eliminate init() in interface

---
+++ src/org/opensourcephysics/media/mov/SmoothPlayable.java
@@ -0,0 +1,16 @@
+package org.opensourcephysics.media.mov;
+
+/**
+ * Interface methods used by XuggleVideo
+ * 
+ * @author hansonr
+ *
+ */
+
+public interface SmoothPlayable extends MovieVideoI {
+
+	boolean isSmoothPlay();
+
+	void setSmoothPlay(boolean b);
+
+}
