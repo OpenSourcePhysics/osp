47c47b524a170191e571be8f7711b8b7d7ba1ef5
Douglas Brown <dobrown@cabrillo.edu>
2021-08-29 15:39:55 -0700

    just correcting misspelling

--- src/org/opensourcephysics/display/OSPRuntime.java
+++ src/org/opensourcephysics/display/OSPRuntime.java
@@ -84,7 +84,7 @@ import swingjs.api.JSUtilI;
 public class OSPRuntime {
 
 	public static final String VERSION = "6.0.0210829"; //$NON-NLS-1$
-	public static final String OSP_PROPERTY_LROCALE = "locale";
+	public static final String OSP_PROPERTY_LOCALE = "locale";
 
 	/**
 	 * An interface with static methods that track implmentinng classes, adding them
