33d7bdb0f61cdb79f8ae89cfe452681194d6c080
hansonr <hansonr@stolaf.edu>
2021-12-19 14:32:43 -0600

    FunctionPanel.instrutions removing mouse listener

--- src/org/opensourcephysics/tools/FunctionPanel.java
+++ src/org/opensourcephysics/tools/FunctionPanel.java
@@ -14,9 +14,6 @@ import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseMotionAdapter;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 
@@ -80,7 +77,6 @@ public class FunctionPanel extends JPanel implements PropertyChangeListener {
 
 	private JButton undoButton;
 	private JButton redoButton;
-	private int varBegin, varEnd;
 	private Icon icon;
 	private boolean haveGUI;
 
