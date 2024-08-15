46348f67b21d261f7189778d60883aa051248df5
bobhanson <hansonr@stolaf.edu>
2024-08-15 12:49:17 -0500

    CursorTest for testing

--- src/demoJS/CursorTest.java
+++ src/demoJS/CursorTest.java
@@ -2,134 +2,188 @@ package demoJS;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Cursor;
 import java.awt.FlowLayout;
 import java.awt.Graphics;
+import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 
-import javax.swing.*;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
+/**
+ * Test of custom cursor in SwingJS. 
+ * 
+ * The cursor and its CSS are created once, on static initiation of this class.
+ * 
+ * 
+ * 
+ */
 public class CursorTest {
+
+	static  {
+		/**
+		 * @j2sNative
+		 * 
+		 *  $("body").append('<link href="https://physlets.org/swingjs/ipad.css" rel="stylesheet" type="text/css">');
+		 * 
+		 * 	$("body").append('<div class="custom-cursor" id="customCursor" style="z-index: 10000000;"></div>');
+		 * 
+		 * 
+		 */
+	}
+
+	
+	class DrawingPanel extends JPanel implements MouseListener, MouseMotionListener {
+
+		public boolean dragging;
+
+		Object customCursor;
+		Cursor myCursor = Cursor.getPredefinedCursor(Cursor.CROSSHAIR_CURSOR);
+
+		DrawingPanel() {
+			setName("drawingPanel");
+			addMouseListener(this);
+			addMouseMotionListener(this);
+			setCursor(myCursor);
+		}
+		@Override
+		public void paint(Graphics g) {
+			super.paint(g);
+			// Get the dimensions of the panel
+			int width = getWidth();
+			int height = getHeight();
+			// Calculate the coordinates for the center of the panel
+			int centerX = width / 2;
+			int centerY = height / 2;
+			// Define the radius of the circle
+			int radius = Math.min(width, height) / 4;
+			// Set the color to red
+			g.setColor(customCursor == null ? Color.YELLOW : dragging ? Color.BLUE : Color.RED);
+			// Draw the circle
+			g.fillOval(centerX - radius, centerY - radius, 2 * radius, 2 * radius);
+		}
+
+		protected void addEventListener() {
+			if (customCursor != null) {
+				customCursor = null;
+				setCursor(myCursor);
+			} else {
+				customCursor = /** @j2sNative $('#customCursor')[0] || */
+						null;
+				setCursor(null);
+			}
+			getParent().repaint();
+		}
+
+		@SuppressWarnings("unused")
+		protected void moveMouseCursor(MouseEvent e, String mode) {
+			// Function to move the custom cursor
+			if (customCursor == null) {
+				return;
+			}
+
+			String display = "block";
+			String background = "rgba(0,255,0,0.3)";
+			switch (mode) {
+			case "move":
+			case "enter":
+				background = "transparent";
+				break;
+			case "drag":
+			case "down":
+				break;
+			case "up":
+			case "exit":
+				background = "transparent";
+				display = "none";
+				break;
+			}
+			/** @j2sNative 
+			var event = e.bdata.jqevent;
+			var x = event.clientX;
+			var y = event.clientY;
+			this.customCursor.style.left = x + "px";
+			this.customCursor.style.top = y + "px";
+        	background && (this.customCursor.style.backgroundColor = background);
+		    display && (this.customCursor.style.display = display);
+				
+			 * 
+			 */
+			getParent().repaint();
+		}
+		
+		@Override
+		public void mouseDragged(MouseEvent e) {
+			dragging = true;
+			moveMouseCursor(e, "drag");
+		}
+
+		@Override
+		public void mouseMoved(MouseEvent e) {
+			moveMouseCursor(e, "move");
+		}
+
+		@Override
+		public void mouseClicked(MouseEvent e) {
+		}
+
+		@Override
+		public void mousePressed(MouseEvent e) {
+			moveMouseCursor(e, "down");
+		}
+
+		@Override
+		public void mouseReleased(MouseEvent e) {
+			moveMouseCursor(e, "up");
+			dragging = false;
+		}
+
+		@Override
+		public void mouseEntered(MouseEvent e) {
+			moveMouseCursor(e, "enter");
+		}
+
+		@Override
+		public void mouseExited(MouseEvent e) {
+			moveMouseCursor(e, "exit");
+		}
+
+	}
 
 	public static void main(String[] args) {
 		JFrame frame = new JFrame("Custom Cursor Example");
 		frame.setLayout(new BorderLayout());
-		JPanel panel = new JPanel();
-		panel.setLayout(new FlowLayout());
-		JLabel label = new JLabel("JavaScript Cursor");
+		DrawingPanel drawingPanel = new CursorTest().new DrawingPanel();
+		frame.add(drawingPanel, BorderLayout.CENTER);
 
+		//JLabel label = new JLabel("JavaScript Cursor");
 		JButton button = new JButton();
-		button.setText("Add EventListener");
+		button.setText("Add/Remove EventListener");
+		button.addActionListener(new ActionListener() {
 
-		button.addActionListener(new ActionListener(){
 			@Override
 			public void actionPerformed(ActionEvent e) {
-			/** @j2sNative console.log('JavaScript addEventListener.'); 
-				debugger; 
-				const customCursor = document.getElementById('customCursor');
-		        
-		        // Function to move the custom cursor
-		        function moveCustomCursor(event) {
-		            let x, y;
-		            if (event.touches) {
-		                x = event.touches[0].clientX;
-		                y = event.touches[0].clientY;
-		            } else {
-		                x = event.clientX;
-		                y = event.clientY;
-		            }
-		            customCursor.style.left = `${x}px`;
-		            customCursor.style.top = `${y}px`;
-		            customCursor.style.display = 'block';
-		          
-		        }
-		
-		        document.addEventListener('mousemove', moveCustomCursor);
-		        document.addEventListener('touchmove', moveCustomCursor);
-		
-		        document.addEventListener('mousedown', () => {
-		            customCursor.style.backgroundColor = 'green'; // Change to your desired click color
-		            console.log('mousedown event');
-		        });
-		        document.addEventListener('mouseup', () => {
-		            customCursor.style.backgroundColor = 'transparent'; // Change back to transparent
-		            console.log('mouseup event');
-		        });
-		
-		        document.addEventListener('touchstart', () => {
-		            customCursor.style.backgroundColor = 'green'; // Change to your desired click color
-		            console.log('touchstart event');
-		        });
-		        document.addEventListener('touchend', () => {
-		            customCursor.style.backgroundColor = 'transparent'; // Change back to transparent
-		            customCursor.style.display = 'none';
-		            console.log('touchend event');
-		        });
-						   
-				*/	
+				drawingPanel.addEventListener();
 			}
-			});
-
-		panel.add(label);
+		});
+		
+		JPanel panel = new JPanel(new FlowLayout());
+		//panel.add(label);
 		panel.add(button);
 
-        JPanel drawingPanel = new JPanel() {
-            @Override
-            protected void paintComponent(Graphics g) {
-                super.paintComponent(g);
-                // Get the dimensions of the panel
-                int width = getWidth();
-                int height = getHeight();
-                // Calculate the coordinates for the center of the panel
-                int centerX = width / 2;
-                int centerY = height / 2;
-                // Define the radius of the circle
-                int radius = Math.min(width, height) / 4;
-                // Set the color to red
-                g.setColor(Color.RED);
-                // Draw the circle
-                g.fillOval(centerX - radius, centerY - radius, 2 * radius, 2 * radius);
-            }
-        };
-        
-        drawingPanel.addMouseListener( new MouseListener() {
-
-			@Override
-			public void mouseClicked(MouseEvent e) {}
-
-			@Override
-			public void mousePressed(MouseEvent e) {
-			/** @j2sNative console.log('JavaScript mousePressed.'); 
-			  customCursor.style.backgroundColor = 'green'; // Change to your desired click color
-			  customCursor.style.display = 'block'; 
-			 */
-			}
-
-			@Override
-			public void mouseReleased(MouseEvent e) {
-			/** @j2sNative console.log('JavaScript mouseReleased.'); 
-			    customCursor.style.backgroundColor = 'transparent'; // Change back to transparent
-		        customCursor.style.display = 'none';
-			 */
-			}
-
-			@Override
-			public void mouseEntered(MouseEvent e) {}
-
-			@Override
-			public void mouseExited(MouseEvent e) {}
-		});
-        
-        frame.add(drawingPanel,BorderLayout.CENTER);
-        
-		frame.add(panel,BorderLayout.SOUTH);
+		frame.add(panel, BorderLayout.SOUTH);
 		frame.setSize(300, 300);
 		frame.setLocationRelativeTo(null);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
-
 	}
-
+	
+	
 }
